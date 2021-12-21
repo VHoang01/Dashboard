643b9930cb18dc7ae07dc9ec2181de5556b290fa
Van HOANG <nguyen-van.hoang@epitech.eu>
2021-12-21 20:52:12 +0100

    added connection between server and mariadb

--- src/back/src/app.js
+++ src/back/src/app.js
@@ -3,6 +3,12 @@ const app = express();
 require('dotenv').config()
 const db = require('./services/db');
 
+db.getConnection().then((con) => {
+   con.query(`SELECT 1 + 1 as solution`).then((row) => {
+      console.log(row);
+   });
+});
+
 app.listen(8080, () => {
     console.log("Server open at: http://localhost:8080");
 })
